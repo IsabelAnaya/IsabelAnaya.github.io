163c32df725293484d6ff86130fc032a13ce2a1d
Isabel Anaya <anaya.isabel.r@gmail.com>
2023-10-26 17:58:42 -0600

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<head>
+    <title>Isabel Anaya</title>
+</head>
+
+<body>
+    <h1>ISABEL ANAYA</h1>
+    <div class="main-card">
+        <p> I'm Isabel, I like messing with code and building things. 
+            I enjoy working on full stack development for software and applications, though I also 
+            have experience with databases, networks, and amateur game development. </p> 
+    </div>
+
+    <div class="main-card">
+        <h2>Experience</h2>
+        <h4 class="italic">Operating Systems Course Assistant ... 09/22 - 05/23</h4>
+        <p>
+            Assisted the Operating Systems class by explaining concepts and material to students in office hours. 
+            Helped troubleshoot issues in code and the Ubuntu environment.
+        </p>
+        <br>
+        <h4 class="italic">Software Developer Intern ... 01/20 - 05/20</h4>
+        <p>
+            Contributed to a research project intending to visualize number sequences. 
+            Designed visualizers with JavaScript, and wrote a user manual explaining parameters and results from the visualizers.
+        </p>
+    </div>
+
+    <div class="main-card">
+        <h2>Skills</h2>
+        <ul class="normal-list">
+            <li >C/C++, C#, JavaScript, Python, HTML/CSS</li>
+            <li>Node.js, Qt, React, Redux, SQL/SQLite</li>
+            <li>Windows, Linux</li>
+        </ul>
+    </div>
+
+    <div class="main-card">
+        <h2 class="text-xl">Projects</h2>
+        <p>Click <a href="/projects">here</a> to see my projects.</p>
+    </div>
+</body>
