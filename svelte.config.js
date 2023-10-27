0ea61932e1c5a26040eec325ecf685e88fd75c98
Isabel Anaya <anaya.isabel.r@gmail.com>
2023-10-26 18:04:57 -0600

    Config changes

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
+			base: process.env.NODE_ENV === 'production' ? '/isabelanaya.github.io' : '',
 		}
 	},
 	preprocess: vitePreprocess()
